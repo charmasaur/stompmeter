f76137fade4aaae7f738bccce975813a039eb1b3
Harry Slatyer <harry.slatyer@gmail.com>
2017-03-08 22:02:03 +1100

    Add a points calculator

---
+++ points_calculator.py
@@ -0,0 +1,12 @@
+def calculate(
+        standing,
+        walking,
+        running,
+        cycling,
+        swimming,
+        stretching):
+    hours_on_feet = standing + walking + running
+    hours_exercising = walking + running + cycling + swimming
+    training_points = (min(hours_on_feet, hours_exercising) + stretching) * 10
+
+    return training_points
