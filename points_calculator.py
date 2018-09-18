d8af6ac2513370452eee5e81ff023ad16c0eb974
Harry Slatyer <harry.slatyer@gmail.com>
2018-09-18 21:14:39 +1000

    Update victorweek calculations for new year

--- points_calculator.py
+++ points_calculator.py
@@ -26,5 +26,9 @@ def is_team_victorweek(week_end_date, team_points):
     # Use threshold of 130 for 10/9/2017 and earlier.
     if week_end_date <= datetime.date(2017, 9, 10):
         return min(team_points) >= 130
-    # Otherwise use 140.
-    return min(team_points) >= 140
+    # Use threshold of 140 for 31/12/2017 and earlier.
+    if week_end_date <= datetime.date(2017, 31, 12):
+        return min(team_points) >= 140
+
+    # Otherwise use 100.
+    return min(team_points) >= 100
