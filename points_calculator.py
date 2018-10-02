4015dc4c21b702d29bae4129c1c822cf2817d0bd
Harry Slatyer <harry.slatyer@gmail.com>
2018-10-02 21:14:48 +1000

    Update points calculations for 2018

--- points_calculator.py
+++ points_calculator.py
@@ -9,6 +9,35 @@ def calculate(
         swimming,
         stretching,
         eatingcarbs):
+    if date <= datetime.date(2017, 12, 31):
+        return _calculate_2017(
+            date,
+            standing,
+            walking,
+            running,
+            cycling,
+            swimming,
+            stretching,
+            eatingcarbs)
+
+    return (standing * 1 +
+            walking * 10 +
+            # Walking over 4 hours gets an extra 5/hour.
+            max(0, walking - 4) * 5 +
+            running * 10 +
+            cycling * 5 +
+            swimming * 5 +
+            stretching * 5)
+
+def _calculate_2017(
+        date,
+        standing,
+        walking,
+        running,
+        cycling,
+        swimming,
+        stretching,
+        eatingcarbs):
     hours_on_feet = standing + walking + running
     hours_exercising = walking + running + cycling + swimming
     training_points = (min(hours_on_feet, hours_exercising) + stretching) * 10
